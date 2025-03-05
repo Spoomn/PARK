f1494ad4f8398c446797d316389f5db5b2f1b574
Spencer Ream <spenceream@gmail.com>
2025-03-05 16:24:11 -0700

    alternate test image/default model
    updated detect.py pipeline

--- mlapp/detect.py
+++ mlapp/detect.py
@@ -1,5 +1,15 @@
-import Ultralytics
+import ultralytics
+import os
 
-TRAINING = "train20"
-model = Ultralytics.YOLO(f"Vehicle-Detection/runs/detect/{TRAINING}/weights/best.pt")
+model = ultralytics.YOLO("yolo11n.pt")
 
+results = model("alt_lot.JPG")
+results[0].show()
+detected_objects = results[0]
+CAR_INDEX = 0
+
+num_cars = sum(1 for obj in detected_objects.boxes.cls if obj == CAR_INDEX)
+print(f"Number of cars detected: {num_cars}")
+os.makedirs("output", exist_ok=True)
+
+results[0].save(f"output/alt_lot1.jpg")
